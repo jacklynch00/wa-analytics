266888fe4405de6edda1bcdf08637e077cda9eb0
jacklynch00 <jrlynch00@gmail.com>
2025-07-24 00:44:22 -0400

    fix some things

--- next.config.js
+++ next.config.js
@@ -1,48 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.google.com',
-        port: '',
-        pathname: '/s2/favicons/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'icons.duckduckgo.com',
-        port: '',
-        pathname: '/ip3/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logo.clearbit.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/auth/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
-    ];
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'www.google.com',
+				port: '',
+				pathname: '/s2/favicons/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'icons.duckduckgo.com',
+				port: '',
+				pathname: '/ip3/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'logo.clearbit.com',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
+	async headers() {
+		return [
+			{
+				source: '/api/auth/(.*)',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: '*',
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET, POST, PUT, DELETE, OPTIONS',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'Content-Type, Authorization',
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
